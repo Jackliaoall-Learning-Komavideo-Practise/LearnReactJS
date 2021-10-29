3ffbb27c958584b8023be9a5832b8bd4ebf12ad5
廖健傑 <a36118@adcmuh.org.tw>
2021-10-29 11:42:24 +0800

    commit Lesson006

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-      <div className="App">
-          <button type="button" className="btn btn-primary">Primary</button>
-          <button type="button" className="btn btn-secondary">Secondary</button>
-          <button type="button" className="btn btn-success">Success</button>
-          <button type="button" className="btn btn-danger">Danger</button>
-          <button type="button" className="btn btn-warning">Warning</button>
-          <button type="button" className="btn btn-info">Info</button>
-          <button type="button" className="btn btn-light">Light</button>
-          <button type="button" className="btn btn-dark">Dark</button>
-          <button type="button" className="btn btn-link">Link</button>
+  // JSX寫法
+  // return (
+  //   <div className="App">
+  //       <h1>I love React.</h1>
+  //   </div>
+  // );
 
-          <ul className="list-group">
-              <li className="list-group-item">An item</li>
-              <li className="list-group-item">A second item</li>
-              <li className="list-group-item">A third item</li>
-              <li className="list-group-item">A fourth item</li>
-              <li className="list-group-item">And a fifth one</li>
-          </ul>
-      </div>
+  // 純JS寫法
+  return React.createElement(
+      'div',
+      { className: 'App' },
+      React.createElement('h1', { className: 'text-danger' }, 'I love React.')
   );
 }
 
