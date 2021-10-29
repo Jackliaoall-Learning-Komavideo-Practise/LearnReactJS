21b46ad36cb45cc14ca87ecb16d8cd33b845e7b5
廖健傑 <a36118@adcmuh.org.tw>
2021-10-29 11:55:56 +0800

    commit Lesson009

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,14 +1,19 @@
 import React, { Fragment } from 'react'
 
 const ItemDetail = () => {
+    const ItemImage = "https://pokemongolive.com/img/posts/anniversaryposter2019.jpg"
+    const ItemTitle = "精灵宝可梦"
+    const ItemContent = "Pokémon GO 三周年啦。"
+    const ItemLink = "https://pokemongolive.com/post/birthday2019/?hl=zh_hant"
+
     return (
         <Fragment>
             <div className="card m-3" style={{ "width": "18rem" }}>
-                <img src="https://pokemongolive.com/img/posts/anniversaryposter2019.jpg" className="card-img-top" alt="" />
+                <img src={ItemImage} className="card-img-top" alt="" />
                 <div className="card-body">
-                <h5 className="card-title">精灵宝可梦</h5>
-                <p className="card-text">Pokémon GO 三周年啦。</p>
-                <a href="https://pokemongolive.com/post/birthday2019/?hl=zh_hant" className="btn btn-primary">走，去看看</a>
+                <h5 className="card-title">{ItemTitle}</h5>
+                <p className="card-text">{ItemContent}</p>
+                <a href={ItemLink} className="btn btn-primary">走，去看看</a>
                 </div>
             </div>
         </Fragment>
