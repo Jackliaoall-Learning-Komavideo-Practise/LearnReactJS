f410a7410ae9893b8b364f994e24131d350f6274
廖健傑 <a36118@adcmuh.org.tw>
2021-11-01 13:00:55 +0800

    commit Lesson011

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import ItemDetail2 from './components/ItemDetail2';
 
 function App() {
-  const itemData = {
-      image: "https://asset.watch.impress.co.jp/img/gmw/docs/1252/730/at_01_m.jpg",
-      title: "免费玩GTA5",
-      content: "Epic Games免费玩GTA5",
-      link: "https://www.epicgames.com/store/ja/p/grand-theft-auto-v",
-  }
-  return (
-      <div className="text-center p-4">
-          <button className="btn btn-danger">确定</button>
-          <hr />
-          <ItemDetail2
-              title={itemData.title}
-              image={itemData.image}
-              content={itemData.content}
-              link={itemData.link}
-          ></ItemDetail2>
-      </div>
-  );
+    const dataList = [
+        {
+            image: "https://pokemongolive.com/img/posts/anniversaryposter2019.jpg",
+            title: "精灵宝可梦",
+            content: "Pokémon GO 三周年啦。",
+            link: "https://pokemongolive.com/post/birthday2019/?hl=zh_hant",
+        },
+        {
+            image: "https://asset.watch.impress.co.jp/img/gmw/docs/1252/730/at_01_m.jpg",
+            title: "免费玩GTA5",
+            content: "Epic Games免费玩GTA5",
+            link: "https://www.epicgames.com/store/ja/p/grand-theft-auto-v",
+        },
+    ]
+    const listItems = dataList.map((itemData, index) =>
+        <ItemDetail2
+            key={index}
+            title={itemData.title}
+            image={itemData.image}
+            content={itemData.content}
+            link={itemData.link}
+        ></ItemDetail2>
+    );
+    return (
+        <div className="text-center p-4">
+            <button className="btn btn-danger">确定</button>
+            <hr />
+            <div className="d-flex">
+                {listItems}
+            </div>
+        </div>
+    );
 }
 
 export default App;
