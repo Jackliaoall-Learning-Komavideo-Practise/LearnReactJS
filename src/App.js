7a4770d9b0a85a5c7dd7103a424f14e542c04174
廖健傑 <a36118@adcmuh.org.tw>
2021-11-04 16:27:50 +0800

    commit Lesson019

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import ItemDetail from './components/ItemDetail';
 import Header from './components/Header';
 import AwsForm from './components/AwsForm';
+import React, { useState } from 'react';
 
 function App() {
     const appData = {
@@ -11,8 +12,27 @@ function App() {
         regionID: 'eu-west-2',
     }
 
+    const [message, setMessage] = useState();
+
     const handlerAppDataEvent = (appdata) => {
-        console.log(appdata)
+        const postdata = JSON.stringify({
+            accessKeyID: appdata.accessKeyID,
+            secretAccessKey: appdata.secretAccessKey,
+            selectRegionID: appdata.regionID,
+        });
+        const res = await fetch('http://127.0.0.1:8080/json', {
+            method: 'POST',
+            body: postdata,
+            headers: {
+                'user-agent': 'Mozilla/99.0 MDN Example',
+                'content-type': 'application/json'
+            },
+        })
+        if (!res.ok) {
+            throw new Error("Is Error");
+        }
+        const data = await res.json();
+        setMessage(JSON.stringify(data.body, {}, 2))
     }
 
     return (
@@ -21,6 +41,9 @@ function App() {
             <div className="p-4">
                 <AwsForm appdata={appData} onSave={handlerAppDataEvent}></AwsForm>
             </div>
+            <pre>
+                {message}
+            </pre>
         </div>
     );
 }
